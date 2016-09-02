9b71ad5c357db6da5e145451dd0eb54a7b21f8d9
Justin Thomas <justinthomas@jtwebs.net>
2016-09-02 17:23:09 -0400

    Modify tracker params, allow arg masses, extend demo

--- scripts/hummingbird_demo.sh
+++ scripts/hummingbird_demo.sh
@@ -39,6 +39,18 @@ read -p "Press [Enter] to move the line"
 rosservice call /multi_mav_services/goFormLine "{roll: 0.0, pitch: 0.0, yaw: 0.0, center: [0.0, 0.0, 1.5], spacing: $SPACING}"
 sleep 1
 
+read -p "Press [Enter] to form circle"
+rosservice call /multi_mav_services/goFormCircle "{roll: 0.0, pitch: 0.0, yaw: 0.0, center: [3.0, 0.0, 2.0], spacing: $SPACING}"
+sleep 1
+
+read -p "Press [Enter] to move circle"
+rosservice call /multi_mav_services/goFormCircle "{roll: 0.0, pitch: 0.4, yaw: 0.0, center: [-4.0, 0.0, 1.0], spacing: $SPACING}"
+sleep 1
+
+read -p "Press [Enter] to make a line"
+rosservice call /multi_mav_services/goFormLine "{roll: 0.0, pitch: -0.2, yaw: 0.0, center: [-3.0, 0.0, 1.5], spacing: $SPACING}"
+sleep 1
+
 read -p "Press [Enter] to land"
 rosservice call /multi_mav_services/land
 sleep 1
